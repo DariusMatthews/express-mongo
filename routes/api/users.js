95bf1d0e4fc30dab25ac0d1a861bc4e717cee522
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-13 17:16:41 -0500

    Figuring out router and mongodb.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,22 @@
 import Express from 'express';
-import { User } from '../../models/userModel.js'
+import { User } from '../../models/userModel.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = Express.Router();
 
 router.get('/', (req, res) => {
-  User.find().then(users => res.json(users));
+  User.find()
+    .then(users => res.json(users));
+});
+
+router.post('/', (req, res) => {
+  const newUser = new User({
+    id: uuidv4(),
+    name: req.body.name,
+  });
+
+  newUser.save()
+    .then(user => res.json(user));
 });
 
 export default router;
